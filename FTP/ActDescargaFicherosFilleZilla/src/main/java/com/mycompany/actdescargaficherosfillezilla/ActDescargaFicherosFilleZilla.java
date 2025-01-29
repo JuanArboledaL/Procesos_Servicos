905f7942939e391ab2ce23def58bed935a293dad
JuanArboledaL <juanarboledalopez@gmail.com>
2025-01-29 11:06:07 +0100

    29/01/2025

--- FTP/ActDescargaFicherosFilleZilla/src/main/java/com/mycompany/actdescargaficherosfillezilla/ActDescargaFicherosFilleZilla.java
+++ FTP/ActDescargaFicherosFilleZilla/src/main/java/com/mycompany/actdescargaficherosfillezilla/ActDescargaFicherosFilleZilla.java
@@ -1,4 +1,8 @@
-package com.mycompany.actividadresuelta44;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actdescargaficherosfillezilla;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -8,25 +12,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.SocketException;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPReply;
 import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
 
+/**
+ *
+ * @author Juanito
+ */
+public class ActDescargaFicherosFilleZilla {
 
-public class GestorFTP {
-    
     private FTPClient clienteFTP;
     private static final String SERVIDOR = "localhost";
     private static final int PUERTO = 21;
     private static final String USUARIO = "usuario";
     private static final String PASSWORD = "12345678";    
-    // Configuracion local de FileZilla Server
-    
-    public GestorFTP() {
-        clienteFTP = new FTPClient();
-    }
-    
-    private void conectar() throws SocketException, IOException {
+
+     private void conectar() throws SocketException, IOException {
         clienteFTP.connect(SERVIDOR,PUERTO);
         int respuesta = clienteFTP.getReplyCode();
         if (!FTPReply.isPositiveCompletion(respuesta)) {
@@ -47,53 +50,35 @@ public class GestorFTP {
     private void desconectar() throws IOException {
         clienteFTP.disconnect();
     }
-    
-    
-    private boolean subirFichero(String path) throws IOException {
-        File ficheroLocal = new File(path);
-        InputStream is = new FileInputStream(ficheroLocal);
-        boolean enviado = clienteFTP.storeFile(ficheroLocal.getName(), is);
-        is.close();
-        return enviado;
-    }
-    
-    
+   
     private boolean descargarFichero(String ficheroRemoto, String pathLocal) throws IOException {
         OutputStream os = new BufferedOutputStream(new FileOutputStream(pathLocal));
         boolean recibido = clienteFTP.retrieveFile(ficheroRemoto,os);
         os.close();
         return recibido;
+        
     }
     
+    public void descargarTodoElContenido() throws IOException{
+    
+        FTPFile ficheros[] = clienteFTP.listFiles();
+        
+        
+    }
     
     public static void main(String[] args) {
+     
+        ActDescargaFicherosFilleZilla gestor = new ActDescargaFicherosFilleZilla();
         
-        GestorFTP gestorFTP = new GestorFTP();
+        try{
         
-        try {
-            gestorFTP.conectar();
+            gestor.conectar();
             System.out.println("Conectado");
             
-           boolean subido = gestorFTP.subirFichero("hola.txt");
-            if (subido) {
-                System.out.println("Fichero subido correctamente");
-            } else {
-                System.out.println("Ha ocurrido un error al intentar subir el fichero");
-            }
             
-            boolean descargado = gestorFTP.descargarFichero("hola.txt", "adios.txt");
-            if (descargado) {
-                System.out.println("Fichero descargado correctamente");
-            } else {
-                System.out.println("Ha ocurrido un error al intentar descargar el fichero");
-            }
-            
-            gestorFTP.desconectar();
-            System.out.println("Desconectado");
-             
-        } catch (Exception e) {
-            System.err.println("Ha ocurrido un error: " + e.getMessage());
+        }catch(Exception e){
+        
+            System.out.println("Ha ocurrido un error: " + e.getMessage());
         }
     }
-    
 }
